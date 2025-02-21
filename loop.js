eb1282a4fd04632677b192de78f672d81abd0a87
Hasib Hasan <mohammodhasibhasan@gmail.com>
2025-02-21 15:31:26 +0600

    first commit

---
+++ loop.js
@@ -0,0 +1,46 @@
+// var nums = [55,66,77,88,99,11,44];
+// for(var i = 0; i<=nums .length; i++){
+// var element =nums[i];
+// console.log(element) 
+
+// }
+
+
+
+
+
+  //  For Loop ------>=     (LEARN TIME) {ASO NIJE KORI✔🥰🥰🌹🥰🥰🥰🥰🥰🥰🥰🥰🥰🥰🥰🥰🥰🥰🥰🥰🥰🥰🥰}
+
+
+
+//   for(var i = 0; 1 <= 10; i++){
+// }
+//     console.log(i);
+  
+
+
+
+    var nums = [1,2,3,4,5,6,7,8,9];
+    for(var i = 0; i < nums.length; i++){
+        var element = nums[i];
+        console.log(element);
+    
+    
+    
+    
+    }
+
+
+    var nums  = [10,20,30,40,50];
+    for( var  i  = 0; i <nums.length; i++){
+        var element  = nums[i]
+        console.log(element);
+    }
+
+
+
+    var nums = [50,60,70,80,90]
+    for(var i  = 0; i < nums.length; i++){
+        var element = nums[i];
+        console.log(element);
+    }
---
+++ while.js
@@ -0,0 +1,40 @@
+    var num = 10;
+    while (num<=15){
+        console.log(num);
+
+    num++
+    }
+
+
+
+
+
+
+
+    //   While loop  --_---    (LEARN TIME) {ASO NIJE KORI✔🥰🥰🌹🥰🥰🥰🥰🥰🥰🥰🥰🥰🥰🥰🥰🥰🥰🥰🥰🥰🥰🥰}
+
+
+
+
+
+
+     var num = 10;
+     while(num<15){
+       console.log(num);
+       num++;
+   }
+
+
+   var num = 541;
+   while(num < 640){
+       console.log(num);
+       num++;
+   }
+
+   var num = 541;
+   while(num <=640){
+       console.log(num);
+       num++
+   }
+
+
